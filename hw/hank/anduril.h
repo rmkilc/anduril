2a64102ac4fe56a8a816526c7b9ed7c2f2d0d56d
rmkilc <rmkilc@gmail.com>
2024-11-20 21:36:02 -0800

    Set Default Aux Colors for Off and Lockout

--- hw/hank/anduril.h
+++ hw/hank/anduril.h
@@ -3,15 +3,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
 
-// config preferences for Hank Wang of Intl-Outdoor (Emisar, Noctigon)
-
-// RGB aux LEDs should use rainbow cycling mode
-// to impress new customers
-// (people usually change it to voltage mode later though)
 #ifdef RGB_LED_OFF_DEFAULT
 #undef RGB_LED_OFF_DEFAULT
 #endif
-#define RGB_LED_OFF_DEFAULT 0x18  // low, rainbow
+#define RGB_LED_OFF_DEFAULT 0x30  // blinking, red
+
+#ifdef RGB_LED_LOCKOUT_DEFAULT
+#undef RGB_LED_LOCKOUT_DEFAULT
+#endif
+#define RGB_LED_LOCKOUT_DEFAULT 0x10  // low, red
 
 // half a second per color in rainbow mode
 //#define RGB_RAINBOW_SPEED 0x03
